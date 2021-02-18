a870a6c8dd3d03f4eeabb32a5ba81e6bed3f760f
Ammar Qureshi <qureshiaq2003@gmail.com>
2021-02-19 01:32:12 +0500

    push

--- Assigment-Java Script.js
+++ Assigment-Java Script.js
@@ -1,7 +1,22 @@
 // const fire=firebase.firestor()
 // console.log(fire)
-const db = document;
+// const db = document;
+const db = firebase.firestore();
+console.log(db);
 
 function setData() {
-  alert("Ammar Qureshi");
+  // alert("Ammar Qureshi");
+  var userData = {
+    title: title,
+    select: select,
+    condition: condition,
+    description: description,
+  };
+  db.collection("/products")
+    .add(userData)
+    .then((res) =>
+     alert("Data Save"))
+    .catch((e) =>
+     console.log("eee", e));
+  console.log(userData);
 }
